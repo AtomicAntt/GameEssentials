c40c7d3b6e92773c2d870e51d09a49499a3601d4
unknown <anthonychi36@gmail.com>
2024-01-24 17:22:28 -0800

    Load and unload methods

--- MainMenu/Main.cs
+++ MainMenu/Main.cs
@@ -15,19 +15,54 @@ public class Main : Node
     public AudioStreamPlayer mainMenuMusic;
     public AudioStreamPlayer inGameMusic;
 
+    // Variables for loading/unloading the game levels ----------
 
+    // Levels is an initialized child node
+    private Node2D _levels;
+
+    // Level instance is the current Node2D scene that will be under the levels node right above, which should contain the level
+    private Node2D _levelInstance;
+
+    // Initialize all child node references ----------
     public override void _Ready()
     {
-        // Initialize all child node references ----------
         mouseHoverSound = GetNode<AudioStreamPlayer>("SFX/MouseHoverSound");
         clickPlaySound = GetNode<AudioStreamPlayer>("SFX/ClickPlaySound");
         quitSound = GetNode<AudioStreamPlayer>("SFX/QuitSound");
         clickSettingsSound = GetNode<AudioStreamPlayer>("SFX/ClickSettingsSound");
 
-        mainMenuMusic = GetNode<AudioStreamPlayer>("Music/")
+        mainMenuMusic = GetNode<AudioStreamPlayer>("Music/MainMenuMusic");
+        inGameMusic = GetNode<AudioStreamPlayer>("Music/InGameMusic");
+
+        _levels = GetNode<Node2D>("Levels");
     }
 
-    // SIGNALS ----------
+    // Methods to handle loading and unloading levels ----------
+
+    public void UnloadLevel()
+    {
+        if (IsInstanceValid(_levelInstance))
+        {
+            _levelInstance.QueueFree();
+        }
+    }
+
+    public void LoadLevel(String levelName)
+    {
+        UnloadLevel();
+
+        // This assumes that the level scene is in a folder called 'Levels' that is under res://
+        String levelPath = "res://Levels/" + levelName + ".tscn";
+        PackedScene levelResource = GD.Load<PackedScene>(levelPath);
+
+        if (levelResource != null)
+        {
+            _levelInstance = levelResource.Instance<Node2D>();
+            _levels.AddChild(_levelInstance);
+        }
+    }
+
+    // SIGNAL METHODS ----------
 
     // BUTTON SIGNALS =========
 
