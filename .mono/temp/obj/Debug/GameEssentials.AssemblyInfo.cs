4fae76dafbe02746e1747228bd54a8c1b760dd1c
unknown <anthonychi36@gmail.com>
2024-01-29 21:32:41 -0800

    Test master volume slider and apply fixes

--- .mono/temp/obj/Debug/GameEssentials.AssemblyInfo.cs
+++ .mono/temp/obj/Debug/GameEssentials.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameEssentials")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be7bcbc3bf743312130a84b40035b79f9ed0c0a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameEssentials")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameEssentials")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
